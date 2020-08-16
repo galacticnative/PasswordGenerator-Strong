086123f7a29f6c6e532347e8f547acb172b09646
galacticnative <68270151+galacticnative@users.noreply.github.com>
2020-08-16 15:08:16 -0700

    updated function call

--- assets/js/script.js
+++ assets/js/script.js
@@ -166,15 +166,19 @@ var passwordSelections = function () {
       result.push(trueOptions[Math.floor(Math.random() * trueOptions.length)]);
     }
     return result.join("");
+
+    passwordSelections();
   }
 
   // Get references to the #generate element
   var generateBtn = document.querySelector("#generate");
+  
   // Write password to the #password input
-  function writePassword() {
+    function writePassword() {
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
     passwordText.value = password;
   }
+
   // Add event listener to generate button
   generateBtn.addEventListener("click", writePassword);
